2d329abfe037e7e3e798071bba24f9edec36df1b
“gefanghe” <aa208208@qq.com>
2023-10-07 15:23:53 +0800

    右侧属性栏，汉化翻译

--- src/components/Bpmn.vue
+++ src/components/Bpmn.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="content">
+<!--    画布区域-->
     <div class="canvas" ref="canvasBpmn" id="canvasBpmn"></div>
+<!--    侧边栏区域-->
+    <div class="properties" ref="properties"></div>
   </div>
 </template>
 <script setup>
 import {ref, reactive, onMounted} from 'vue'
+//bpmn汉化
+import translate from '../assets/translations.js'
+// 引入bpmn
 import BpmnModeler from 'bpmn-js/lib/Modeler'
+//引入bpmn侧边栏
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+} from 'bpmn-js-properties-panel';
 import {xmlStr} from '../mock/xmlStr'
 const canvasBpmn = ref()
+const properties = ref()
 const state = reactive({
   //bpmn建模
   bpmnModeler: null,
@@ -22,8 +34,19 @@ const createNewDiagram = () => {
 }
 const init = () => {
   const canvas = canvasBpmn.value
+  const proper = properties.value
   state.bpmnModeler = new BpmnModeler({
-    container: canvas
+    container: canvas,
+    //控制板
+    propertiesPanel:{
+      parent:proper
+    },
+    additionalModules: [
+      BpmnPropertiesPanelModule,
+      BpmnPropertiesProviderModule,
+      { translate: ['value', translate] }
+    ]
+
   })
   createNewDiagram()
 }
@@ -43,6 +66,13 @@ onMounted(() => {
   .canvas {
     width: 100%;
     height: 100%;
+  }
+  .properties{
+    position: absolute;
+    right: 0;
+    top: 20px;
+    padding: 30px;
+    background-color: #eeeeee;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// bpmn整体样式
 import 'bpmn-js/dist/assets/diagram-js.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'
 import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css'
+// bpmn右边工具栏样式
+// import 'bpmn-js-properties-panel/dist/assets/bpmn-js-properties-panel.css'
 createApp(App).mount('#app')
